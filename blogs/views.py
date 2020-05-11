95221ce2d26c57cc547f890508f59de928617213
oBuiNhatDuy <bui.nhat.duy@sun-asterisk.com>
2020-05-11 16:50:47 +0700

    Add tempalte

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = "home.html"
-
-class AboutPageView(TemplateView):
-    template_name = "about.html"
+    context_object_name = "all_posts_list"
