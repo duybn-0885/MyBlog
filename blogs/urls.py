18d97c173c694e4e21a8d185c6753b6384679b90
oBuiNhatDuy <bui.nhat.duy@sun-asterisk.com>
2020-05-11 16:05:59 +0700

    Add static pagee

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from .views import HomePageView, AboutPageView
 
-from . import views
-
-url_patterns = [
-    path('', views.index, name='index')
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "home.html"
 
-def index(request):
-    return HttpResponse("Hello. This is my blog")
+class AboutPageView(TemplateView):
+    template_name = "about.html"
